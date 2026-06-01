3fc39b23b443edc5ca09dff03a2df04bff049927
arnavanand2005 <arnavanand2005@gmail.com>
2026-06-01 22:27:47 +0530

    Add routing and create basic page components for Home, About, Profile, Signin, and Signup

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+
+export default function App() {
+  return (
+    <div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </Router>
+    </div>
+  )
+}
