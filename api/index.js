60b5f69b80e399b7fb7c68554e28e63819c3224c
arnavanand2005 <arnavanand2005@gmail.com>
2026-06-02 15:37:42 +0530

    Add MongoDB connection and dotenv configuration

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import nodemon from 'nodemon';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).
+then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000 ...');
-    });
-
+    console.log('Server is running on port 3000');
+});
