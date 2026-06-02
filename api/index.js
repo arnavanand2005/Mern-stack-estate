17f116dd8b845f8dc717e67ce2c27bc0e6cf9037
arnavanand2005 <arnavanand2005@gmail.com>
2026-06-02 19:30:11 +0530

    Add user model with schema definition and update .gitignore for .env

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB', err));
 
 const app = express();
-
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
