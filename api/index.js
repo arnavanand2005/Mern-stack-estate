2bc094b060a3064f4db1550ad074e6aa4f5e78d6
arnavanand2005 <arnavanand2005@gmail.com>
2026-06-03 10:27:09 +0530

    Refactor signup controller to use error handling middleware and improve error responses; enhance API structure and formatting

--- api/index.js
+++ api/index.js
@@ -1,19 +1,33 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).
-then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Could not connect to MongoDB', err));
+mongoose.connect(process.env.MONGO)
+.then(() => console.log("Connected to MongoDB"))  
+.catch((err) => console.log(err));
 
 const app = express();
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-app.use(express.json());
-app.use('/api/user', userRouter); 
-app.use('/api/auth', authRouter);
+    console.log("Server is running on localhost:3000")
+})
+
+app.use(express.json())
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err,req,res,next) => {
+    success : false;
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    });
+})
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -3,19 +3,20 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     username : {
         type : String,
-        required: true,
-        unique: true
+        unique : true,
+        required : true
     },
     email : {
         type : String,
-        required: true,
-        unique: true
-    }, 
+        unique : true,
+        required : true
+    },
     password : {
         type : String,
-        required: true
-    }}, {timestamps: true});
+        required : true
+    }}, {timestamps : true});
 
-const User = mongoose.model('User', userSchema);
- 
-export default User; 
+const User = mongoose.model("User", userSchema);
+
+export default User;
+
