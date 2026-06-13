ca17af848ee1926c95226b7b617a7d6f30e168e0
arnavanand2005 <arnavanand2005@gmail.com>
2026-06-13 16:40:03 +0530

    Add image slider to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-
-
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 function Listing() {
     const [listing, setListing] = useState(null)
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
 
     const params = useParams()
@@ -16,21 +18,43 @@ function Listing() {
             const data = await res.json()
 
             if(data.success === false){
-                setLoading(false);
                 setError(true);
+                setLoading(false);
               return;  
             }
-            setListing(data)
-        };;
-
+            setListing(data);
+            setLoading(false);
+            setError(false)
+        };
         
         fetchListing()
-    })
-  return (
-   <div>
-    {listing && listing.name}
-   </div>
-  )
-}
+    },[params.listingId])
+
+    return (
+        <main>
+          {loading && <p className='text-center text-3xl my-7'>Loading...</p>}
+          {error && <p className='text-center text-3xl my-7'>Something Went Wrong...</p>}
+          
+          {listing && !error && !loading && (
+            <div>
+              <Swiper modules={[Navigation]} navigation>
+                {listing.imageUrls && listing.imageUrls.map((url) => (
+                  <SwiperSlide key={url}>
+                    <div 
+                      className="h-[500px] w-full bg-center bg-no-repeat" 
+                      style={{ backgroundImage: `url(${url})`, backgroundSize: 'cover' }}
+                    ></div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+      
+              <h1 className="text-2xl font-bold p-6 text-slate-800">
+                {listing.name}
+              </h1>
+            </div>
+          )}
+        </main>
+      );
+    }
 
 export default Listing
